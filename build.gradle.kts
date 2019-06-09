4b52cb8d986b45d51110218c1f6753f9b9d943ea
Sola <dev@sola.love>
2019-06-10 03:34:52 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+group = "love.sola.copier"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.3.31"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("no.tornado:tornadofx:1.7.19")
+    implementation("com.github.thomasnield:rxkotlinfx:2.2.2")
+    implementation("net.samuelcampos:usbdrivedetector:2.0.4")
+}
